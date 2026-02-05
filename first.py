1b4afabb876050ccdda4bcd684698a3ddb0066f7
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-02-05 12:07:21 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,17 @@
-print("Seelan")
+print("Senthan")
+print('senthan')
+print(17)
+name="Senthan"
+print(name)
+age=19
+print(age)
+
+print (type(age))
+print (type(name))
+print("my name is"+ name)
+print("my age is ",age)
+print("my name is",name)
+print("my name is"+str(age))
+print(f"my name is {name} \n my age is {age}\n")
+print("my name is {}\n my age is{} \n ".format(name,age))
+print("my name is {1}\n my age is{0} \n ".format(age,name))
