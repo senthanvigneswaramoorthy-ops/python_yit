5ff7354d2a93af9d18804934328b487399a47e60
Priyanka2633 <balakrishnanpriyanka30@gmail.com>
2026-02-10 09:54:43 +0530

    operators

---
+++ assignment operators.py
@@ -0,0 +1,16 @@
+x=12
+print(x)
+x+=5
+print("x+=5:",x)
+x-=5
+print("x-=5:",x)
+x*=5
+print("x*=5:",x)
+x/=5
+print("x/=5:",x)
+x//=5
+print("x//=5:",x)
+x%=5
+print("x%=5:",x)
+x**=5
+print("x**=5:",x)
---
+++ comparision operators1.py
@@ -0,0 +1,8 @@
+x=24
+y=21
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
