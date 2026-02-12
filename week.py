e008d0aa35ebeba63a74eb5e696a7becf5e8b87e
senthanvigneswaramoorthy-ops <senthanvigneswaramoorthy@gmail.com>
2026-02-12 06:47:03 +0530

    senthan

---
+++ week.py
@@ -0,0 +1,18 @@
+x=int(input("Enter a number between 0 and 7:"))
+if 7>=x>0:
+    if x==1:
+        print("Monday")
+    if x==2:
+        print("Tuesday")
+    if x==3:
+        print("Wednesday")
+    if x==4:
+        print("THurseday")
+    if x==5:
+        print("Friday")
+    if x==6:
+        print("Saturday")
+    if x==7:
+        print("Sunday")
+else:
+    print("Enter a valid number")
